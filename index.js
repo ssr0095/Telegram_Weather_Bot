05102bc50c8b9b7627b19fd58ebf07228d3e86d4
ssr <ssrxx10@gmail.com>
2024-05-26 01:11:38 +0530

    comit

---
+++ index.js
@@ -0,0 +1,31 @@
+const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '6674930533:AAFaCTHKl4IQeF23qkga8I7Ur8Grbg8DuJM';
+
+const bot = new TelegramBot(token, {polling: true});
+
+
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id;
+  const userInput = msg.text;
+
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=58250d9a3a42a8bd7e23e01ed001c54a`
+    );
+    const data = response.data;
+    const weather = data.weather[0].description;
+    const temperature = data.main.temp - 273.15;
+    const city = data.name;
+    const humidity = data.main.humidity;
+    const pressure = data.main.pressure;
+    const windSpeed = data.wind.speed;
+    const message = `The weather in ${city} is ${weather} with a temperature of ${temperature.toFixed(2)}°C. The humidity is ${humidity}%, the pressure is ${pressure}hPa, and the wind speed is ${windSpeed}m/s.`;
+
+    bot.sendMessage(chatId, message);
+  } catch (error) {
+    bot.sendMessage(chatId, "City doesn't exist.");
+  }
+});

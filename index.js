f0fe9abb9e070df3185ff15a3c14c17b793244b0
ssr <ssrssr0095@gmail.com>
2024-06-17 17:53:44 +0530

    start update

--- index.js
+++ index.js
@@ -16,29 +16,35 @@ const token = ''+`${process.env.TEL_BOT_TOKEN}`;
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Welcome! Enter a City to explore 🏭');
-});
+// bot.onText(/\/start/, (msg) => {
+//   const chatId = msg.chat.id;
+//   bot.sendMessage(chatId, 'Welcome! Enter a City to explore 🏭');
+// });
 
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
   const userInput = msg.text;
 
   try {
-    const response = await axios.get(
+    if(userInput == "/start"){
+      bot.sendMessage(chatId, 'Welcome! Enter a City to explore 🏭');
+    }
+    else{
+      const response = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${process.env.WEATHER_API}`
-    );
-    const data = response.data;
-    const weather = data.weather[0].description;
-    const temperature = data.main.temp - 273.15;
-    const city = data.name;
-    const humidity = data.main.humidity;
-    const pressure = data.main.pressure;
-    const windSpeed = data.wind.speed;
-    const message = `The weather in ${city} is ${weather} with a temperature of ${temperature.toFixed(2)}°C. The humidity is ${humidity}%, the pressure is ${pressure}hPa, and the wind speed is ${windSpeed}m/s.`;
-
-    bot.sendMessage(chatId, message);
+      );
+      const data = response.data;
+      const weather = data.weather[0].description;
+      const temperature = data.main.temp - 273.15;
+      const city = data.name;
+      const humidity = data.main.humidity;
+      const pressure = data.main.pressure;
+      const windSpeed = data.wind.speed;
+      const message = `The weather in ${city} is ${weather} with a temperature of ${temperature.toFixed(2)}°C. The humidity is ${humidity}%, the pressure is ${pressure}hPa, and the wind speed is ${windSpeed}m/s.`;
+  
+      bot.sendMessage(chatId, message);
+    }
+    
   } catch (error) {
     bot.sendMessage(chatId, "City doesn't exist.");
   }
